afb7f3afa541e6948a8a0ffb591ad3698c40f8f5
kimjeff49 <jeffkkim612@gmail.com>
2024-10-18 10:57:10 -0400

    removing some stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="h-full w-full flex justify-center items-center">
-    <section class="w-1/2 h-1/4">
-      <h1 class="text-2xl text-center">Hello!</h1>
+  <main class="h-screen w-full flex justify-center items-center">
+    <section class="">
+      <h1 class="text-2xl text-center font-bold">Hello! My name is Jeff</h1>
     </section>
+    <section></section>
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
@@ -7,8 +9,17 @@ export default {
         'retro-orange': '#e8850d',
         'retro-blue': '#085cd4',
         'retro-tan': '#ead777',
-        'retro-black': '#231e0b'
+        'retro-black': '#231e0b',
+        'retro-off-white': '#fffff2'
+      },
+      fontFamily: {
+        lora: ['Lora', 'sans-serif'],
+        barlow: ['Barlow', 'serif']
       }
+    },
+    fontFamily: {
+      sans: ['Barlow', ...fontFamily.sans],
+      display: ['Lora', ...fontFamily.sans]
     }
   },
   plugins: []
